17b0fbecff7fae3c8ba6d5fe373a7ad4aabb3e98
RicNorris <ricardojorgepmoreira@gmail.com>
2025-04-25 11:54:31 +0100

    Moved local env to online

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,6 +1,16 @@
 const { createClient } = require("redis");
+const dotenv = require("dotenv");
 
-const redis = createClient();
+// Load environment variables
+dotenv.config();
+
+const redis = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
 
 redis.on("error", (err) => console.error("Redis Client Error", err));
 
