22f0d1e37afec65c313529890e2a51c8d6f1babb
RicNorris <ricardojorgepmoreira@gmail.com>
2025-05-02 15:20:23 +0100

    Added display of the Guildstone Room image

---
+++ src/utils/roomImages.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'Guildstone Room': "https://github.com/RicNorris/Guild-Of-Mages/blob/main/public/rooms/guildstone_room.png?raw=true",
+  default:
+    "https://github.com/RicNorris/Guild-Of-Mages/blob/main/public/rooms/guildstone_room.png?raw=true",
+};
